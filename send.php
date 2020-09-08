f39436cabbcebbfd059457e0eec20607cf35143b
Rusdiv <64499427+Rusdiv@users.noreply.github.com>
2020-09-08 19:53:40 +0300

    master

---
+++ send.php
@@ -0,0 +1,43 @@
+<?php
+
+$name = $_POST['fio'];
+$mail = $_POST['email'];
+$tel = $_POST['tel'];
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'poctovikruslan@gmail.com';                     // SMTP username
+    $mail->Password   = '4fgnDd7NRu';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('poctovikruslan@gmail.com', 'Mailer');
+    $mail->addAddress('ruslan.ru225@mail.ru', 'Joe User');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка';
+    $mail->Body    = '
+    <p>FIO:${name}</p>
+    <p>Mail:${mail}</p>
+    <p>Tel:${tel}</p>
+    ';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
