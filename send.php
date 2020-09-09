cdad7441e1a38f03ad3d541b82b69d39641d1714
Rusdiv <64499427+Rusdiv@users.noreply.github.com>
2020-09-09 15:28:05 +0300

    master

--- send.php
+++ send.php
@@ -18,14 +18,14 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'istina115@mail.ru';                     // SMTP username
+    $mail->Username   = 'poctovikruslan@gmail.com';                     // SMTP username
     $mail->Password   = '4fgnDd7NRu';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('istina115@mail.ru', 'Mailer');
-    $mail->addAddress('ruslan.ru225@mail.ru', 'Joe User');     // Add a recipient
+    $mail->setFrom('poctovikruslan@gmail.com', 'Mailer');
+    $mail->addAddress('istina115@mail.ru', 'Joe User');     // Add a recipient
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
